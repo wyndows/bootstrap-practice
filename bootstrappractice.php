e75aa70e6e83e0faad6f1e9d15053c03310b2ed2
Marlan Ball <wyndows2k@earthlink.net>
2016-05-09 05:49:40 -0600

    practicing bootstrap

---
+++ bootstrappractice.php
@@ -0,0 +1,153 @@
+<!DOCTYPE html>
+<html lang="en">
+	<!-- using code from Rochelle Lewis -->
+	<head>
+		<meta charset="UTF-8">
+
+		<!-- sets IE rendering to IE-EDGE -->
+		<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+
+		<!-- sets viewport width to device width, scaling 1:1 -->
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+
+		<!-- Latest compiled and minified CSS -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+
+		<!-- Optional theme -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous">
+
+		<!-- HTML5 shiv and Respond.js for IE8 support of HTML5 elements and media queries -->
+		<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+		<!--[if lt IE 9]>
+		<script src="//oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+		<script src="//oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+		<![endif]-->
+
+		<!-- OUR CUSTOM CSS -->
+		<link rel="stylesheet" href="css/style.css" type="text/css">
+
+		<!-- jQuery -->
+		<script src="https://code.jquery.com/jquery-2.2.3.min.js"   integrity="sha256-a23g1Nt4dtEYOj7bR+vTu7+T8VP13humZFBJNIYoEJo="   crossorigin="anonymous"></script>
+
+		<!-- Latest compiled and minified JavaScript -->
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+
+		<!-- ADD YOUR JAVASCRIPT/JQUERY DOWN HERE -->
+
+		<title>Land of Oz</title>
+	</head>
+	<body class="sfooter">
+		<div class="sfooter-content">
+
+			<header>
+				<div class="container">
+					<!-- begin bootstrap navbar -->
+					<!-- Brand and toggle get grouped for better mobile display -->
+					<div class="navbar-header">
+						<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#main-menu" aria-expanded="false">
+							<span class="sr-only">Toggle navigation</span>
+							<i class="fa fa-th fa-2x fa-spin" aria-hidden="true"></i>
+						</button>
+						<a class="navbar-brand" href="#">Wizard of Oz</a>
+					</div>
+
+					<!-- Collect the nav links, forms, and other content for toggling -->
+					<div class="collapse navbar-collapse" id="main-menu">
+						<ul class="nav navbar-nav navbar-right">
+							<li><a href="/">Kansas</a></li>
+							<li class="dropdown">
+								<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Click Your Heels <span class="caret"></span></a>
+								<ul class="dropdown-menu">
+									<li><a href="http://wyn-tech.com">Hey Hey for the Monkeys</a></li>
+									<li><a href="https://amazon.com">Ruby Slippers</a></li>
+								</ul>
+							</li>
+						</ul>
+					</div><!-- /.navbar-collapse -->
+				</div>
+			</header>
+
+			<main>
+				<div class="container">
+					<div class="row">
+
+						<!--welcome section-->
+						<div class="col-xs-12 well text-center">
+							<h1>Welcome to the Land of Oz</h1>
+							<p>Keep an eye out for ruby slippers...</p>
+						</div>
+
+						<!--4 column content section-->
+						<div class="row">
+							<div class="col-md-3">
+								<div class="well">
+									<h4>Dorothy</h4>
+									<p>After this she stood upon both feet and cried in a loud voice: "Ziz-zy, zuz-zy, zik!" Now the charm began to work. The sky was darkened, and a low rumbling sound was heard in the air. There was a rushing of many wings, a great chattering and laughing, and the sun came out of the dark sky to show the Wicked Witch surrounded by a crowd of monkeys, each with a pair of immense and powerful wings on his shoulders. One, much bigger than the others, seemed to be their leader. He flew close to the Witch</p>
+								</div>
+							</div>
+							<div class="col-md-3">
+								<div class="well">
+									<h4>Toto</h4>
+									<p>After this she stood upon both feet and cried in a loud voice: "Ziz-zy, zuz-zy, zik!" Now the charm began to work. The sky was darkened, and a low rumbling sound was heard in the air. There was a rushing of many wings, a great chattering and laughing, and the sun came out of the dark sky to show the Wicked Witch surrounded by a crowd of monkeys, each with a pair of immense and powerful wings on his shoulders. One, much bigger than the others, seemed to be their leader. He flew close to the Witch</p>
+								</div>
+							</div>
+							<div class="col-md-3">
+								<div class="well">
+									<h4>Cowardly Lion</h4>
+									<p>After this she stood upon both feet and cried in a loud voice: "Ziz-zy, zuz-zy, zik!" Now the charm began to work. The sky was darkened, and a low rumbling sound was heard in the air. There was a rushing of many wings, a great chattering and laughing, and the sun came out of the dark sky to show the Wicked Witch surrounded by a crowd of monkeys, each with a pair of immense and powerful wings on his shoulders. One, much bigger than the others, seemed to be their leader. He flew close to the Witch</p>
+								</div>
+							</div>
+							<div class="col-md-3">
+								<div class="well">
+									<h4>Scarecrow</h4>
+									<p>After this she stood upon both feet and cried in a loud voice: "Ziz-zy, zuz-zy, zik!" Now the charm began to work. The sky was darkened, and a low rumbling sound was heard in the air. There was a rushing of many wings, a great chattering and laughing, and the sun came out of the dark sky to show the Wicked Witch surrounded by a crowd of monkeys, each with a pair of immense and powerful wings on his shoulders. One, much bigger than the others, seemed to be their leader. He flew close to the Witch</p>
+								</div>
+							</div>
+						</div>
+
+						<!--2 column content section-->
+						<div class="row">
+							<div class="col-md-6">
+								<div class="well 2col">
+									<h4>Tinman</h4>
+									<p>Why, anybody can have a brain. That's a very mediocre commodity. Every pusillanimous creature that crawls on the Earth or slinks through slimy seas has a brain. Back where I come from, we have universities, seats of great learning, where men go to become great thinkers. And when they come out, they think deep thoughts and with no more brains than you have. But they have one thing you haven't got: a diploma.</p>
+								</div>
+							</div>
+							<div class="col-md-6">
+								<div class="well 2col">
+									<h4>Glenda</h4>
+									<p>Why, anybody can have a brain. That's a very mediocre commodity. Every pusillanimous creature that crawls on the Earth or slinks through slimy seas has a brain. Back where I come from, we have universities, seats of great learning, where men go to become great thinkers. And when they come out, they think deep thoughts and with no more brains than you have. But they have one thing you haven't got: a diploma.</p>
+								</div>
+							</div>
+						</div>
+					</div><!--/.row-->
+				</div><!--/.container-->
+			</main>
+
+		</div>
+
+		<footer>
+			<div class="container">
+				<div class="row">
+					<div class="col-md-6">
+						<div class="foot-content">
+							All right, I'll go in there for Dorothy. Wicked Witch or no Wicked Witch, guards or no guards, I'll tear them apart.
+						</div>
+					</div>
+					<div class="col-md-6">
+						<div class="foot-content">
+							I may not come out alive, but I'm going in there. There's only one thing I want you fellows to do
+						</div>
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-xs-12">
+						<div class="copyright text-center">
+							&copy; 1900 L. Frank Baum
+						</div>
+					</div>
+				</div>
+			</div>
+		</footer>
+	</body>
+</html>
